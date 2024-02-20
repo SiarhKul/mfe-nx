4cf05f30b8191e02828fd74c6d3b20e463681293
Siarhei <siarhkul@gmail.com>
2024-02-20 22:01:56 +0100

    feat: Add remote navbar component

--- apps/host/module-federation.config.ts
+++ apps/host/module-federation.config.ts
@@ -14,7 +14,7 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  remotes: ['shop', 'cart', 'about'],
+  remotes: ['shop', 'cart', 'about', 'navbar'],
 };
 
 export default config;
---
+++ apps/host/src/remotes.d.ts
@@ -0,0 +1,7 @@
+// Declare your remote Modules here
+
+declare module 'about/Module';
+declare module 'card/Module';
+declare module 'shop/Module';
+
+declare module 'navbar/Module';
