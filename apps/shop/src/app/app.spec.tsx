bfb65d3c03a30c3255d3a5d72c47be11f916803a
siarhkul <siarhkul@gmail.com>
2024-04-12 18:12:08 +0200

    Revert "chore:Break UT"
    This reverts commit 10b7646bae584801cb6401cbd3cfe0bf4d2ba1eb.

--- apps/shop/src/app/app.spec.tsx
+++ apps/shop/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome shop/i)).not.toBeTruthy();
+    expect(getByText(/Welcome shop/i)).toBeTruthy();
   });
 });
