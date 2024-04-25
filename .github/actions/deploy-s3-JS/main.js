731fb13c41ab89037bf7654f3e1ec0ca6ab8ffac
siarhkul <siarhkul@gmail.com>
2024-04-25 07:47:27 +0200

    feat: JS custom action

---
+++ .github/actions/deploy-s3-JS/main.js
@@ -0,0 +1,9 @@
+const core = require('@actions/core');
+const github = require('@actions/exec');
+const exec = require('@actions/github');
+
+function run() {
+  core.notice('Heloo from my custom js actions');
+}
+
+run();
