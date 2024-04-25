7a16effdc9d45a10490bf1f3164480f6febf5970
siarhkul <siarhkul@gmail.com>
2024-04-25 21:49:43 +0200

    feat: Custom JS actons1

--- .github/actions/deploy-s3-JS/main.js
+++ .github/actions/deploy-s3-JS/main.js
@@ -1,9 +1,10 @@
-const core = require('@actions/core');
-const github = require('@actions/exec');
-const exec = require('@actions/github');
-
-function run() {
-  core.notice('Heloo from my custom js actions');
-}
-
-run();
+// const core = require('@actions/core');
+// const github = require('@actions/exec');
+// const exec = require('@actions/github');
+//
+// function run() {
+//   core.notice('Heloo from my custom js actions');
+// }
+//
+// run();
+console.log('333333333333333333');
