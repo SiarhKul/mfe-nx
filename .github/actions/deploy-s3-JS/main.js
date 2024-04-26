7de3d9b956a8ff54469552c91a31925778a5b926
siarhkul <siarhkul@gmail.com>
2024-04-26 19:48:37 +0200

    feat: Custom JS S3 bucket2

--- .github/actions/deploy-s3-JS/main.js
+++ .github/actions/deploy-s3-JS/main.js
@@ -9,8 +9,8 @@ function runDeployStaticToS3() {
 
   console.log('CONTEXT_GIT_HUB', github.context);
 
-  // const s3URL = `s3://${bucket}`;
-  // exec.exec(`aws s3 sync ${distFolder} ${s3URL} --region ${region}`);
+  const s3URL = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3URL} --region ${region}`);
 
   core.notice('Heloo from my custom js actions');
 }
