12ebe11350c6dc0d79a0af1e98528bbd382e83d1
siarhkul <siarhkul@gmail.com>
2024-04-26 20:10:52 +0200

    feat: Custom JS S3 bucket3

--- .github/actions/deploy-s3-JS/main.js
+++ .github/actions/deploy-s3-JS/main.js
@@ -1,18 +1,20 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
+// http://mfe-nx-statics.s3-website.eu-central-1.amazonaws.com
 
 function runDeployStaticToS3() {
   const bucket = core.getInput('bucket', { required: true });
   const region = core.getInput('region', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
-
-  console.log('CONTEXT_GIT_HUB', github.context);
-
+  const URL = `http://${bucket}.s3-website.${region}.amazonaws.com`;
   const s3URL = `s3://${bucket}`;
+
   exec.exec(`aws s3 sync ${distFolder} ${s3URL} --region ${region}`);
 
-  core.notice('Heloo from my custom js actions');
+  core.setOutput(URL);
+
+  console.log('CONTEXT_GIT_HUB', github.context);
 }
 
 runDeployStaticToS3();
