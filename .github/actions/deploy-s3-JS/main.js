8b89d0453bbfecb8f79f34a20e9c71e33e4da9ba
siarhkul <siarhkul@gmail.com>
2024-04-26 08:30:45 +0200

    feat: Custom JS S3 bucket

--- .github/actions/deploy-s3-JS/main.js
+++ .github/actions/deploy-s3-JS/main.js
@@ -1,9 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/exec');
-const exec = require('@actions/github');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
 
-function run() {
+function runDeployStaticToS3() {
+  const bucket = core.getInput('bucket', { required: true });
+  const region = core.getInput('region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  console.log('CONTEXT_GIT_HUB', github.context);
+
+  // const s3URL = `s3://${bucket}`;
+  // exec.exec(`aws s3 sync ${distFolder} ${s3URL} --region ${region}`);
+
   core.notice('Heloo from my custom js actions');
 }
 
-run();
+runDeployStaticToS3();
